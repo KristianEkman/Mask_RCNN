5f247e13006166e2141fefe3181c67d73f03f355
KristianEkman <kristian.ekman.swe@gmail.com>
2019-10-06 09:04:02 +0200

    colon

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2196,7 +2196,7 @@ class MaskRCNN():
             loss = (
                 tf.reduce_mean(layer.output, keepdims=True)
                 * self.config.LOSS_WEIGHTS.get(name, 1.))
-            if not hasattr(self.keras_model, 'metrics_tensors')
+            if not hasattr(self.keras_model, 'metrics_tensors'):
                 self.keras_model.metrics_tensors = []
             self.keras_model.metrics_tensors.append(loss)
 
